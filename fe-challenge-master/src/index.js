2930509fffb745cf753fa76e838510494697afa9
Max Lareau <max@maxlareau.com>
2022-01-12 20:09:45 -0500

    Added favicon, implemented menu show/hide functionality

--- fe-challenge-master/src/index.js
+++ fe-challenge-master/src/index.js
@@ -5,7 +5,7 @@ const navbar_itemgroup = document.querySelector('.nav-item-group');
 const content = document.querySelector('.container-vertical');
 
 // Set menu visibility state state
-menu.style.visibility = 'hidden';
+// menu.style.visibility = 'hidden';
 
 const menuClickHandler = () => {
 
@@ -15,25 +15,34 @@ const menuClickHandler = () => {
     if (menu.style.visibility === 'hidden') {
 
         // Hide content
-        content.style.visibility = 'hidden';
+        content.style.visibility = 'collapse';
+        content.style.overflow = 'hidden';
 
         // Show the menu
-        menu.style.visibility = 'visible'
+        menu.style.visibility = 'visible';
 
         // Bring navbar to front and style for vertical viewing
-        navbar.classList.add('navbar-vertical');
-        navbar_itemgroup.classList.add('nav-item-group-vertical');
+        navbar.style.zIndex = '2';
+        navbar.style.visibility = 'visible';
+
+        // Hide navbar item group for larger screens
+        navbar_itemgroup.style.visibility = 'hidden';
 
     } else {
 
         // Show content
         content.style.visibility = 'visible';
+        content.style.overflow = 'auto';
 
         // Hide menu
-        menu.style.visibility = 'hidden'
+        menu.style.visibility = 'hidden';
 
         // Revert navbar back to original state
         navbar.classList.remove('navbar-vertical');
+        navbar.style.paddingRight = '0';
+
+        // Show navbar item group
+        navbar_itemgroup.style.visibility = 'visible';
 
     }
 };
